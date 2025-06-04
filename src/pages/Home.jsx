d6d47fab2f1f2832687c2320042649b3f6a6d150
Njaka02 <njakaharilantoandriamijeri@gmail.com>
2025-06-04 11:38:51 +0300

    [ADD] ADD background particuls js

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,23 @@
+import { useContext } from "react";
 import { Navbar } from "../components/Navbar";
 import { Footer } from "../components/Footer";
+import { ThemeContext } from "../utils/context";
 import { AwardsSection } from "../components/Award";
 import { Experiences } from "../components/Experiences";
 import { ThemeToggle } from "../components/ThemeToogle";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
+import ParticlesComponent from "../components/Particlesjs";
 import { SkillsSection } from "../components/SkillsSection";
 import { StarBackground } from "../components/StarBackground";
 import { ProjectsSection } from "../components/ProjectsSection";
 
 function Home() {
+  const { theme } = useContext(ThemeContext);
+
   return (
     <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {theme === "light" && <ParticlesComponent id="particles" />}
       <ThemeToggle />
       <StarBackground />
       <Navbar />
